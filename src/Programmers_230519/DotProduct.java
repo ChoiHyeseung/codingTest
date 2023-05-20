536dcc4332162514806fa656c34f02692fcb4ce2
Choi, Hyeseung <qorgus31@naver.com>
2023-05-21 04:14:05 +0900

    [혜승] 프로그래머스 230521

---
+++ src/Programmers_230519/DotProduct.java
@@ -0,0 +1,22 @@
+package Programmers_230519;
+
+public class DotProduct {
+    public static int solution(int[] a, int[] b){
+        int sum = 0;
+        for(int i = 0; i < a.length; i++){
+            sum += a[i]*b[i];
+
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4};
+        int b[] = {-3,-1,0,2};
+
+        int c[] = {-1,0,1};
+        int d[] = {1, 0, -1};
+
+        System.out.println(solution(a, b));
+        System.out.println(solution(c, d));
+    }
+}
